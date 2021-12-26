432767e66a1a6ad374a22004b01e088faef1447c
Tihomir Heidelberg <tihomir.heidelberg@cascination.com>
2021-12-26 14:29:36 +0000

    cm pelet set mods

--- custom_components/peltec/sensors/generic_sensors_cm_pelet_set.py
+++ custom_components/peltec/sensors/generic_sensors_cm_pelet_set.py
@@ -1,26 +1,35 @@
-from homeassistant.const import (
-    DEVICE_CLASS_TEMPERATURE,
-    TEMP_CELSIUS,
-)
+from homeassistant.const import DEVICE_CLASS_TEMPERATURE, TEMP_CELSIUS, TIME_MINUTES
 
 CM_PELET_SET_SENSOR_TEMPERATURES = {
+    "B_Tk1": [
+        TEMP_CELSIUS,
+        "mdi:thermometer",
+        DEVICE_CLASS_TEMPERATURE,
+        "Boiler Temperature",
+    ],
+    "C1B_Tpol1": [
+        TEMP_CELSIUS,
+        "mdi:thermometer",
+        DEVICE_CLASS_TEMPERATURE,
+        "Circuit 1 Flow Temperature",
+    ],
     "C2B_Tpol1": [
         TEMP_CELSIUS,
         "mdi:thermometer",
         DEVICE_CLASS_TEMPERATURE,
-        "Circuit Flow 1 Temperature",
+        "Circuit 2 Flow Temperature",
     ],
     "B_Tak1": [
         TEMP_CELSIUS,
         "mdi:thermometer",
         DEVICE_CLASS_TEMPERATURE,
-        "Buffer Tank Temparature Up",
+        "Buffer Tank Up",
     ],
     "B_Tak2": [
         TEMP_CELSIUS,
         "mdi:thermometer",
         DEVICE_CLASS_TEMPERATURE,
-        "Buffer Tank Temparature Down",
+        "Buffer Tank Down",
     ],
     "B_Tva1": [
         TEMP_CELSIUS,
@@ -33,8 +42,28 @@ CM_PELET_SET_SENSOR_TEMPERATURES = {
 
 CM_PELET_SET_SENSOR_MISC = {
     "B_KONF_STR": ["", "mdi:information", None, "Setup"],
-    "C1B_CircType": ["", "mdi:information", None, "Heating type"],
+    "B_netMon": ["", "mdi:remote", None, "Remote Start Enabled"],
+    "B_cmsr100": ["", "mdi:information", None, "Pellet Tank Level"],
+    "C1B_CircType": ["", "mdi:information", None, "Circuit 1 Heating Type"],
+    "C2B_CircType": ["", "mdi:information", None, "Circuit 2 Heating Type"],
+    "C1B_misC": ["", "mdi:pipe-valve", None, "Circ1 Mix-Valve Closing"],
+    "C1B_misO": ["", "mdi:pipe-valve", None, "Circ1 Mix-Valve Opening"],
+    "C2B_misC": ["", "mdi:pipe-valve", None, "Circ2 Mix-Valve Closing"],
+    "C2B_misO": ["", "mdi:pipe-valve", None, "Circ2 Mix-Valve Ipening"],
     "B_Pk": ["", "mdi:pump", None, "Boiler Pump"],
+    "C1B_P": ["", "mdi:pump", None, "Circuit 1 Pump"],
+    "C1B_onOff": ["", "mdi:information", None, "Circuit 1 State"],
+    "C2B_P": ["", "mdi:pump", None, "Circuit 2 Pump"],
+    "C2B_onOff": ["", "mdi:information", None, "Circuit 2 State"],
+    "B_fan": ["", "mdi:fan", None, "Heater Fan State"],
+    "B_gri": ["", "mdi:fire", None, "Heater State"],
+    "CNT_0": [TIME_MINUTES, "mdi:fire", None, "Heater State Counter"],
+    "CNT_3": ["", "mdi:fire", None, "Heater Start Counter (?)"],
+    "CNT_4": [TIME_MINUTES, "mdi:fan", None, "Heater Fan Counter"],
+    "CNT_5": [TIME_MINUTES, "mdi:fire", None, "Heater State Counter"],
+    "CNT_6": ["", "mdi:fire", None, "Heater ukljuèenje (?)"],
+    "CNT_7": [TIME_MINUTES, "mdi:information", None, "Pellet Transporter Counter"],
+    "CNT_8": [TIME_MINUTES, "mdi:pump", None, "Boiler Pump Counter"],
 }
 
 CM_PELET_SET_GENERIC_SENSORS = {
